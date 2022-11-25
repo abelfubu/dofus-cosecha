1d9e7bcd55998ad3f3a16e050e50838b965a5b82
Abel de la Fuente <abel.delafuente@feverup.com>
2022-11-25 11:25:57 +0100

    feat(chore): fix navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,11 @@ import { Route, RouterModule } from '@angular/router';
 
 const routes: Route[] = [
   {
+    path: '',
+    redirectTo: 'harvest',
+    pathMatch: 'full',
+  },
+  {
     path: 'harvest',
     loadComponent: () =>
       import('./pages/harvest/havest.component').then((c) => c.HavestComponent),
@@ -10,7 +15,7 @@ const routes: Route[] = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
