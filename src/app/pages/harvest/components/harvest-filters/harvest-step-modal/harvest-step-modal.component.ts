07eeae2e09116e9b79af36a7f876564ec3191444
Abel de la Fuente <abel.delafuente@feverup.com>
2023-03-16 22:07:43 +0100

    ✨ feat(harvest-step-modal): filter data only on modal close

--- src/app/pages/harvest/components/harvest-filters/harvest-step-modal/harvest-step-modal.component.ts
+++ src/app/pages/harvest/components/harvest-filters/harvest-step-modal/harvest-step-modal.component.ts
@@ -1,8 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { Observable, take, tap } from 'rxjs';
-import { ButtonComponent } from 'src/app/shared/ui/button/button.component';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ButtonComponent } from '@shared/ui/button/button.component';
 import { HarvestStore } from '../../../harvest.store';
 import { HarvestSteps } from '../../../models/harvest-steps';
 
@@ -16,23 +16,30 @@ import { HarvestSteps } from '../../../models/harvest-steps';
 export class HarvestStepModalComponent {
   form!: FormGroup;
   store!: HarvestStore;
-  steps$!: Observable<boolean[]>;
+  steps!: boolean[];
 
   private readonly formBuilder = inject(FormBuilder);
+  private readonly dialogRef = inject(MatDialogRef);
+  private readonly data = inject<boolean[]>(MAT_DIALOG_DATA);
 
   ngOnInit(): void {
-    this.steps$ = this.store.steps$.pipe(
-      take(1),
-      tap((steps) => this.initializeStepsForm(steps)),
-    );
+    this.steps = this.data;
+    this.initializeStepsForm(this.data);
   }
 
   setAllControls(steps: boolean[], value: boolean): void {
     this.form.setValue(HarvestSteps.mapToFormValue(steps, value));
   }
 
+  applySteps(): void {
+    this.dialogRef.close(this.form.value);
+  }
+
+  closeModal(): void {
+    this.dialogRef.close();
+  }
+
   private initializeStepsForm(steps: boolean[]): void {
     this.form = this.formBuilder.group(HarvestSteps.mapToFormGroup(steps));
-    this.store.steps(this.form.valueChanges);
   }
 }
--- src/app/pages/harvest/components/harvest-table/harvest-table-header/harvest-table-header.component.ts
+++ src/app/pages/harvest/components/harvest-table/harvest-table-header/harvest-table-header.component.ts
@@ -2,6 +2,7 @@ import { ComponentType } from '@angular/cdk/portal';
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { filter, switchMap, take } from 'rxjs';
 import { ButtonComponent } from 'src/app/shared/ui/button/button.component';
 import { HarvestStore } from '../../../harvest.store';
 import { HarvestStepModalComponent } from '../../harvest-filters/harvest-step-modal/harvest-step-modal.component';
@@ -20,10 +21,17 @@ export class HarvestTableHeaderComponent {
   private readonly matDialog = inject(MatDialog);
   private readonly harvestStore = inject(HarvestStore);
 
-  openFilter<T extends { store: unknown }>(component: ComponentType<T>) {
-    const instance = this.matDialog.open(component, {
-      panelClass: 'background',
-    });
-    instance.componentInstance.store = this.harvestStore;
+  openFilter<T>(component: ComponentType<T>) {
+    this.harvestStore.steps$
+      .pipe(
+        take(1),
+        switchMap((steps) =>
+          this.matDialog
+            .open(component, { data: steps, panelClass: 'background' })
+            .afterClosed(),
+        ),
+        filter(Boolean),
+      )
+      .subscribe((data) => this.harvestStore.steps(data));
   }
 }
